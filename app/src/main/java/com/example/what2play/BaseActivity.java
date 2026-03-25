1ad6c107ff65fc29d4e87d4e5894c2ac1b54367d
Lancelot <valcurion369@gmail.com>
2026-03-25 17:33:25 +0100

    some logs

--- app/src/main/java/com/example/what2play/BaseActivity.java
+++ app/src/main/java/com/example/what2play/BaseActivity.java
@@ -3,6 +3,7 @@ package com.example.what2play;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,10 +14,13 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void attachBaseContext(Context newBase) {
         SharedPreferences prefs = newBase.getSharedPreferences("Settings", MODE_PRIVATE);
-        String lang = prefs.getString("lang", "en");
 
+        //Get selected language and create new local
+        String lang = prefs.getString("lang", "en");
         Locale locale = new Locale(lang);
         Locale.setDefault(locale);
+
+        Log.d("BaseActivity", "New local with langage: " + lang);
 
         Configuration config = new Configuration();
         config.setLocale(locale);
